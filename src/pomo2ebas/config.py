fe70e75b996d2f3b701dcc743ad414c800b996aa
Robert Gebauer <robert.gebauer@gmx.de>
2023-10-20 21:04:31 +0200

    Made yaml position configurable.

--- src/pomo2ebas/config.py
+++ src/pomo2ebas/config.py
@@ -1,16 +1,18 @@
 """ Supplies default parameter values. """
 import yaml
 
+
 class Defaults(object):
-    """ Defaults store that supplies defaults for commands and parameters. """
-    def __init__(self):
-        with open(r"yaml\defaults.yaml", 'r') as cf:
+    """Defaults store that supplies defaults for commands and parameters."""
+
+    def __init__(self, config_file=r"yaml\defaults.yaml"):
+        with open(config_file, "r") as cf:
             self.defaults = yaml.load(cf, Loader=yaml.FullLoader)
 
     def get_default(self, command: str, parameter: str):
-        """ Returns a default value for the given command and parameter or None if no such value exists. """
-        if (command in self.defaults.keys()):
-            if (parameter in self.defaults[command]):
+        """Returns a default value for the given command and parameter or None if no such value exists."""
+        if command in self.defaults.keys():
+            if parameter in self.defaults[command]:
                 return self.defaults[command][parameter]
 
         return None
