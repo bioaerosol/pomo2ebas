e6e377264f9fdd1de65fc4aada340084e8c5f829
salashhab <salashhab@ua.es>
2023-10-23 16:37:36 +0200

    Fixed EBAS expects UTC timestamps, however, BAA500 delivers Europe/Berlin

--- src/pomo2ebas/monitors.py
+++ src/pomo2ebas/monitors.py
@@ -7,16 +7,29 @@ Reads from xml or imput files for each station
 import xml.etree.ElementTree as ElementTree
 from dateutil.parser import parse
 from .pollen import Pollen_Concentration
-
+import datetime
+import pytz
 
 class BAA500(object):
     """ Concrete implementation for Hund monitor BAA500. """
 
     @staticmethod
-    def timestamp_to_datetime(timestamp: str):
+    def timestamp_to_datetime(self, timestamp: str):
         """ Returns a given timestamp string from BAA500 XML file as a datetime (for example datetime.datetime(2023, 9, 10, 3, 0, 3)). """
         ts = parse(timestamp)
+        ts = self.convert_datetime_timezone(ts, "Europe/Berlin", "UTC")
+        ts = parse(ts)
         return ts
+        
+
+    def convert_datetime_timezone(self, dt, from_time_zone, to_time_zone):
+        from_time_zone = pytz.timezone(from_time_zone)
+        to_time_zone = pytz.timezone(to_time_zone)
+
+        dt = from_time_zone.localize(dt)
+        dt = dt.astimezone(to_time_zone)
+        dt = dt.strftime("%Y-%m-%d %H:%M:%S")
+        return dt
     
     @staticmethod
     def measure_accuracy(pollen_name, root):
@@ -37,8 +50,8 @@ class BAA500(object):
         
         
         # uncomment for testing
-        #tree = ElementTree.parse(r"C:\Users\Tofahito\Desktop\ZAUM\EBAS\data\samples\more samples\20230910030003_A223329\20230910030003_A223329\analysis\polle-ad_01-20230910030003-pmon-00025-A223329-xml.xml") 
-        #root = tree.getroot()
+        # tree = ElementTree.parse(r"C:\Users\Tofahito\Desktop\ZAUM\EBAS\data\samples\more samples\20230910030003_A223329\20230910030003_A223329\analysis\polle-ad_01-20230910030003-pmon-00025-A223329-xml.xml") 
+        # root = tree.getroot()
         
         xml = ""
         for line in io_wrapper:
@@ -61,8 +74,8 @@ class BAA500(object):
                 pollen[pollen_name] = Pollen_Concentration(float(konzentration.get("Pollenkonzentration")),accuracy)
 
         d = {}
-        d["start"] = BAA500.timestamp_to_datetime(start.text)
-        d["end"] = BAA500.timestamp_to_datetime(end.text)
+        d["start"] = BAA500.timestamp_to_datetime(self,start.text)
+        d["end"] = BAA500.timestamp_to_datetime(self,end.text)
         d["pollen"] = pollen
             
         return d
